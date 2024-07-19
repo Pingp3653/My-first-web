3d34f986a0f1294d1b82e3bf5930ea4f53154f33
Ping <pingp3653@gmail.com>
2024-07-19 14:53:13 +0700

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 urlpatterns = [
     path('',home,name="home-page"),
     path('contact/',contact,name="contact-page"),
-    path('about/',about,name="about-page")
+    path('about/',about,name="about-page"),
+
+    path('register',Regidter,name='register'),
+    path('login',Login,name='login'),
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post 
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 def home(request):
     all_post = Post.objects.all()
     
@@ -15,3 +15,54 @@ def contact(request):
 
 def about(request):
     return render(request, "myapp/about.html")
+
+def Regidter(request):
+
+    context = {}
+    
+    if request.method == 'POST':
+        data = request.POST.copy()
+        name = data.get('name')
+        email = data.get('email')
+        password = data.get('password')
+
+        check = User.objects.filter(username=email)
+
+        if len(check) == 0:
+           newuser=User()
+           newuser.username = email
+           newuser.first_name = name
+           newuser.set_password(password)
+           newuser.save()
+           context['success'] = 'success'
+        else:
+            context['usertaken'] = 'usertaken'
+
+
+
+    return render(request,'myapp/register.html',context)
+
+def Login(request):
+
+    context = {}
+    
+    if request.method == 'POST':
+        data = request.POST.copy()
+        email = data.get('email')
+        password = data.get('password')
+
+        check = User.objects.filter(username=email)
+
+        if len(check) == 0:
+           context['nouser'] = 'nouser'
+        else:
+            try:
+                user = authenticate(username=email,password=password)
+                login(request,user)
+                print('login completed')
+            except:
+                context['wrongpassword'] = 'wrongpassword'
+
+
+
+    return render(request,'myapp/login.html',context)
